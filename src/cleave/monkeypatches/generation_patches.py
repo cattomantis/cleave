4b28fc4127b55b8a26385617d9c1cf6aa9f431fb
nasheedyasin <nasheed.ny@gmail.com>
2025-03-03 12:32:02 -0800

    Update generation_patches.py
    checkpoint

--- src/cleave/monkeypatches/generation_patches.py
+++ src/cleave/monkeypatches/generation_patches.py
@@ -3,10 +3,23 @@ from transformers import (
     GenerationConfig, LogitsProcessorList,
     StoppingCriteriaList, PreTrainedModel,
 )
+from transformers.utils import (
+    ModelOutput,
+    is_accelerate_available,
+    is_hqq_available,
+    is_optimum_quanto_available,
+    is_torchdynamo_compiling
+)
 from transformers.generation.streamers import BaseStreamer
 from transformers.generation import GenerateDecoderOnlyOutput
+from transformers.generation.utils import GenerationMode, logger
 
+import inspect
+import warnings
 from typing import Optional, List, Callable, Union
+
+
+SUPPORTED_GENERATION_MODES = [GenerationMode.GREEDY_SEARCH, GenerationMode.SAMPLE, GenerationMode.CONTRASTIVE_SEARCH]
 
 
 @torch.inference_mode()
@@ -28,6 +41,7 @@ def latent_mode_generate(
     to the model to generate the next token at time step `t`.
     
     Note: This is an experimental feature and only works with `GenerationMode GREEDY_SEARCH` and `SAMPLE`.
+    CONTRASTIVE_SEARCH
 
     Args & Returns:
         Same description for the args as described here:
@@ -45,4 +59,132 @@ def latent_mode_generate(
     model.generate = latent_mode_generate
     ```
     """
-    ...
+    # 1. Handle `generation_config` and kwargs that might update it, and validate the `.generate()` call
+    self._validate_model_class()
+    tokenizer = kwargs.pop("tokenizer", None)  # Pull this out first, we only use it for stopping criteria
+    assistant_tokenizer = kwargs.pop("assistant_tokenizer", None)  # only used for assisted generation
+
+    generation_config, model_kwargs = self._prepare_generation_config(generation_config, **kwargs)
+    self._validate_model_kwargs(model_kwargs.copy())
+    self._validate_assistant(assistant_model, tokenizer, assistant_tokenizer)
+
+    # 2. Set generation parameters if not already defined
+    logits_processor = logits_processor if logits_processor is not None else LogitsProcessorList()
+    stopping_criteria = stopping_criteria if stopping_criteria is not None else StoppingCriteriaList()
+
+    accepts_attention_mask = "attention_mask" in set(inspect.signature(self.forward).parameters.keys())
+    requires_attention_mask = "encoder_outputs" not in model_kwargs
+    kwargs_has_attention_mask = model_kwargs.get("attention_mask", None) is not None
+
+    # 3. Define model inputs
+    inputs_tensor, model_input_name, model_kwargs = self._prepare_model_inputs(
+        inputs, generation_config.bos_token_id, model_kwargs
+    )
+    batch_size = inputs_tensor.shape[0]
+
+    if batch_size > 1:
+        raise NotImplementedError("Latent mode generation does not support batch_size>1 (yet!)")
+
+    device = inputs_tensor.device
+    self._prepare_special_tokens(generation_config, kwargs_has_attention_mask, device=device)
+
+    if self.config.is_encoder_decoder:
+        raise NotImplementedError("Latent mode generation does not support encoder-decoder models (yet!)")
+
+    # We always use KV Cache
+    generation_config.use_cache = True
+
+    if not kwargs_has_attention_mask and requires_attention_mask and accepts_attention_mask:
+        model_kwargs["attention_mask"] = self._prepare_attention_mask_for_generation(
+            inputs_tensor, generation_config, model_kwargs
+        )
+
+    elif kwargs_has_attention_mask:
+        if model_input_name == "input_ids" and len(model_kwargs["attention_mask"].shape) > 2:
+            raise ValueError("`attention_mask` passed to `generate` must be 2D.")
+
+    # 5. Prepare `input_ids` which will be used for auto-regressive generation
+    input_ids = inputs_tensor if model_input_name == "input_ids" else model_kwargs.pop("input_ids")
+
+    if generation_config.token_healing:
+        input_ids = self.heal_tokens(input_ids, tokenizer)
+
+    # 6. Prepare `max_length` depending on other stopping criteria.
+    input_ids_length = input_ids.shape[-1]
+    has_default_max_length = kwargs.get("max_length") is None and generation_config.max_length is not None
+    has_default_min_length = kwargs.get("min_length") is None and generation_config.min_length is not None
+    generation_config = self._prepare_generated_length(
+        generation_config=generation_config,
+        has_default_max_length=has_default_max_length,
+        has_default_min_length=has_default_min_length,
+        model_input_name=model_input_name,
+        inputs_tensor=inputs_tensor,
+        input_ids_length=input_ids_length,
+    )
+
+    # If the model supports `logits_to_keep` in forward(), set it to 1 to avoid computing the whole
+    # logit matrix. This can save a lot of memory during the first forward pass. Note that assisted decoding
+    # dynamically overrides this value as it can need more than the last token logits
+    if self._supports_logits_to_keep() and "logits_to_keep" not in model_kwargs:
+        model_kwargs["logits_to_keep"] = 1
+
+    self._validate_generated_length(generation_config, input_ids_length, has_default_max_length)
+
+    # 7. Prepare the cache.
+    # - `model_kwargs` may be updated in place with a cache as defined by the parameters in `generation_config`.
+    # - different models have a different cache name expected by the model (default = "past_key_values")
+    # - `max_length`, prepared above, is used to determine the maximum cache length
+    if (
+        inputs_tensor.shape[1] != input_ids_length
+        and model_input_name == "inputs_embeds"
+        and not self.config.is_encoder_decoder
+    ):
+        max_cache_length += inputs_tensor.shape[1]
+    self._prepare_cache_for_generation(
+        generation_config, model_kwargs, assistant_model, batch_size, max_cache_length, device
+    )
+
+    # 8. determine generation mode
+    generation_mode = generation_config.get_generation_mode(assistant_model)
+    if generation_mode not in SUPPORTED_GENERATION_MODES:
+        raise NotImplementedError(
+            f"Generation mode {generation_mode} not supported (yet!). Supported modes are {SUPPORTED_GENERATION_MODES}"
+        )
+
+    if generation_config.num_beams > 1:
+        raise NotImplementedError("Latent mode generation does not support beam search (yet!)")
+
+    if streamer is not None and ():
+        raise NotImplementedError(
+            "`streamer` cannot be used with latent generation (yet!)."
+        )
+
+    if not is_torchdynamo_compiling() and self.device.type != input_ids.device.type:
+        warnings.warn(
+            "You are calling .generate() with the `input_ids` being on a device type different"
+            f" than your model's device. `input_ids` is on {input_ids.device.type}, whereas the model"
+            f" is on {self.device.type}. You may experience unexpected behaviors or slower generation."
+            " Please make sure that you have put `input_ids` to the"
+            f" correct device by calling for example input_ids = input_ids.to('{self.device.type}') before"
+            " running `.generate()`.",
+            UserWarning,
+        )
+
+    # 9. prepare logits processors and stopping criteria
+    prepared_logits_processor = self._get_logits_processor(
+        generation_config=generation_config,
+        input_ids_seq_length=input_ids_length,
+        encoder_input_ids=inputs_tensor,
+        prefix_allowed_tokens_fn=prefix_allowed_tokens_fn,
+        logits_processor=logits_processor,
+        device=inputs_tensor.device,
+        model_kwargs=model_kwargs,
+        negative_prompt_ids=negative_prompt_ids,
+        negative_prompt_attention_mask=negative_prompt_attention_mask,
+    )
+    prepared_stopping_criteria = self._get_stopping_criteria(
+        generation_config=generation_config, stopping_criteria=stopping_criteria, tokenizer=tokenizer, **kwargs
+    )
+
+    # Set model_kwargs `use_cache` so we can use it later in forward runs
+    model_kwargs["use_cache"] = generation_config.use_cache
